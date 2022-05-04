3212d99267b15ca9faa8ee6be13064c32d65e5fd
Samuel Pastva <sam.pastva@gmail.com>
2022-05-04 13:47:45 +0200

    Update the symbolic operators implementation with a full range of possible options.

--- src/bin/reach_bench.rs
+++ src/bin/reach_bench.rs
@@ -1,14 +1,13 @@
-// A very basic benchmark for testing the performance of reachability procedures.
-
 use biodivine_lib_param_bn::biodivine_std::traits::Set;
 use biodivine_lib_param_bn::symbolic_async_graph::SymbolicAsyncGraph;
 use biodivine_lib_param_bn::BooleanNetwork;
 use std::convert::TryFrom;
-use std::io::Read;
+
+// A very basic benchmark for testing the performance of reachability procedures.
 
 fn main() {
-    let mut buffer = String::new();
-    std::io::stdin().read_to_string(&mut buffer).unwrap();
+    let args = std::env::args().collect::<Vec<_>>();
+    let buffer = std::fs::read_to_string(&args[1]).unwrap();
 
     let model = BooleanNetwork::try_from(buffer.as_str()).unwrap();
     let stg = SymbolicAsyncGraph::new(model.clone()).unwrap();
@@ -18,9 +17,13 @@ fn main() {
         let mut set = universe.pick_vertex();
         // Reach back
         'bwd: loop {
-            if set.symbolic_size() > 1_000 {
-                println!("BWD progress: {}", set.symbolic_size());
-            }
+            //if set.symbolic_size() > 1_000 {
+            println!(
+                "BWD progress: {} {}",
+                set.symbolic_size(),
+                set.approx_cardinality()
+            );
+            //}
             for var in model.variables().rev() {
                 //let step = stg.var_pre(var, &set).minus(&set);
                 let step = stg.var_pre_out(var, &set);
@@ -33,11 +36,16 @@ fn main() {
         }
         // Reach front
         'fwd: loop {
-            if set.symbolic_size() > 1_000 {
-                println!("FWD progress: {}", set.symbolic_size());
-            }
+            //if set.symbolic_size() > 1_000 {
+            println!(
+                "FWD progress: {} {}",
+                set.symbolic_size(),
+                set.approx_cardinality()
+            );
+            //}
             for var in model.variables().rev() {
-                let step = stg.var_post(var, &set).minus(&set);
+                //let step = stg.var_post(var, &set).minus(&set);
+                let step = stg.var_post_out(var, &set);
                 if !step.is_empty() {
                     set = set.union(&step);
                     continue 'fwd;
